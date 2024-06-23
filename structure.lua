de0c13f67ff4e445ace932ab0b82c5229941cfcc
Asha Ravilla <sriashalatha369@gmail.com>
2024-06-22 21:27:45 -0500

    included the Data Ingestion code

---
+++ structure.lua
@@ -0,0 +1,22 @@
+/Youtube-Content-Insights
+|-- /src
+|   |-- api_ingestion.py
+|   |-- etl_job.py
+|   |-- data_transformation.py
+|-- /data
+|   |-- /raw
+|   |-- /processed
+|   |-- /transformed
+|-- /notebooks
+|   |-- exploratory_analysis.ipynb
+|   |-- data_visualization.ipynb
+|-- /docs
+|   |-- project_overview.md
+|   |-- data_lake_design.md
+|   |-- data_transformation.md
+|   |-- data_visualization.md
+|-- /dashboards
+|   |-- tableau_dashboard.twb
+|-- README.md
+|-- LICENSE
+|-- .gitignore
